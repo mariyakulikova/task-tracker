3500ca6520cf6f78a7dc9399291417ae0024cfed
Maria <bmf_@mail.ru>
2020-04-21 20:11:25 +0300

    add simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthComponent} from './auth/auth.component';
+import {WeekPreviewComponent} from './week-preview/week-preview.component';
+import {TimerComponent} from './timer/timer.component';
+import {EditComponent} from './edit/edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent
+  },
+  {
+    path: 'calendar',
+    component: WeekPreviewComponent
+  },
+  {
+    path: 'timer',
+    component: TimerComponent
+  },
+  {
+    path: 'edit',
+    component: EditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
