2da2578806b3e83d476fb53877112da57bc619df
Maria <bmf_@mail.ru>
2020-05-09 16:48:23 +0300

    add form validation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,37 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../servises/auth.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
 
-  email: string;
-  password: string;
+  form: FormGroup;
   hasError = false;
   errorMessage: string;
   title = 'task-tracker';
 
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    private fb: FormBuilder
+  ) {
+  }
 
-  signUp() {
-    this.authService.signUp(this.email, this.password)
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: [null, [
+        Validators.required,
+        Validators.email
+      ]],
+      password: [null, [
+        Validators.required,
+        Validators.minLength(6)
+      ]]
+    });
+  }
+
+  singUp() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.authService.signUp(this.form.value.email, this.form.value.password)
       .catch(err => {
         this.hasError = true;
         this.errorMessage = err.message;
       });
-    this.email = this.password = '';
+    this.form.reset();
   }
 
   login() {
-    this.authService.login(this.email, this.password)
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.authService.login(this.form.value.email, this.form.value.password)
       .catch(err => {
         this.hasError = true;
         this.errorMessage = err.message;
       });
-    this.email = this.password = '';
+    this.form.reset();
   }
 
   singInWithGoogle() {
@@ -40,5 +65,31 @@ export class AuthComponent {
         this.hasError = true;
         this.errorMessage = err.message;
       });
+  }
+
+  ifControlInvalid(controlName: string): boolean {
+    const control = this.form.get(controlName);
+    return control.invalid && control.touched;
+  }
+
+  getErrorMessage(controlName: string): string {
+    const control = this.form.get(controlName);
+    const errors = control.errors;
+
+    if (!errors) {
+      return '';
+    }
+
+    if (errors.minlength) {
+      return `minimal password length - ${errors.minlength.requiredLength}`;
+    }
+
+    if (errors.required) {
+      return `${controlName} is required`;
+    }
+
+    if (errors.email) {
+      return 'e-mail is badly formatted';
+    }
   }
 }
