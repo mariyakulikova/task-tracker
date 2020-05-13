3ca96a217c6eb7867ec6048219b5b15834c810b7
Maria <bmf_@mail.ru>
2020-05-13 13:07:23 +0300

    fix problem: sidebar is shown when return to auth page from calendar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {AuthService} from '../servises/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent{
+export class SidebarComponent {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   onLogOut() {
     this.auth.logout();
   }
 
   showLogOut(): boolean {
-    return this.auth.isAuthenticated;
+    return this.auth.isAuthenticated && this.router.url !== '/';
   }
 }
